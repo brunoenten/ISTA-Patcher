b53aa8e269b8db7bffbfce553ea8d047ed2f9989
Bruno Enten <bruno@enten.me>
2023-12-02 14:54:35 +0100

    Update PatchUtils.Toyota.cs

--- ISTA-Patcher/Core/PatchUtils.Toyota.cs
+++ ISTA-Patcher/Core/PatchUtils.Toyota.cs
@@ -11,34 +11,5 @@ using dnlib.DotNet;
 /// </summary>
 internal static partial class PatchUtils
 {
-    [ToyotaPatch]
-    public static int PatchToyotaWorker(ModuleDefMD module)
-    {
-        return module.PatchFunction(
-            "\u0042\u004d\u0057.Rheingold.\u0054\u006f\u0079\u006f\u0074\u0061.Worker.ToyotaWorker",
-            "VehicleIsValid",
-            "(System.String)System.Boolean",
-            DnlibUtils.ReturnTrueMethod
-        );
-    }
-
-    [ToyotaPatch]
-    public static int PatchIndustrialCustomerManager(ModuleDefMD module)
-    {
-        return module.PatchFunction(
-            "\u0042\u004d\u0057.Rheingold.CoreFramework.IndustrialCustomer.Manager.IndustrialCustomerManager",
-            "IsIndustrialCustomerBrand",
-            "(System.String)System.Boolean",
-            DnlibUtils.ReturnTrueMethod
-        );
-    }
-
-    [ToyotaPatch]
-    public static int PatchGTSLicenseManager(ModuleDefMD module)
-    {
-        return module.PatcherGetter(
-            "Toyota.GTS.ForIsta.GTSLicenseManager",
-            "LicenseStatus",
-            DnlibUtils.ReturnZeroMethod);
-    }
+   
 }
